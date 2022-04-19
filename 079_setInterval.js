b16f55f8b9f648b3f7582b00f28f218733f1c889
Charles An <mucollabo@gmail.com>
2022-04-19 18:20:23 +0900

    complete until 080

---
+++ 079_setInterval.js
@@ -0,0 +1,3 @@
+setInterval(() => console.log('hello'), 1000);    // 1초에 1번씩 'hello' 출력하기
+const printBye = () => console.log('bye');    // 2초에 1번씩 bye 출력하기
+setInterval(printBye, 2000);
---
+++ 080_setTimeout.js
@@ -0,0 +1,2 @@
+// 3초 후에 hello를 한 번 출력하고 종료하기
+setTimeout(() => console.log('hello'), 3000);
