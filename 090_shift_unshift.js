d4f25e3dfb0472de5e2ea8737485cbf66569c159
Charles An <mucollabo@gmail.com>
2022-04-21 00:44:32 +0900

    complete until 090

---
+++ 090_shift_unshift.js
@@ -0,0 +1,11 @@
+const destination = ['London', 'Paris', 'Roma'];
+
+console.log(destination);
+
+// shift
+console.log(destination.shift());
+console.log(destination);
+
+// unshift
+destination.unshift('NewYork');
+console.log(destination);
